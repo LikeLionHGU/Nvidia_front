cf857f6cb4ced0086eb850d3b57d0444a92fcd55
Gyuho-Han <0203ho@naver.com>
2025-08-13 12:13:22 +0900

    edit: 배포는 나중에 하자..

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Nvidia_front/'
+  // base: '/Nvidia_front/' 배포는 나중에 하자..
 })
 
