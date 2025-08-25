ac4de1eb7e2ecb05037cc1b55e3496d34c633321
JongHyun <dkrehd0519@naver.com>
2025-08-25 15:49:54 +0900

    feat: 더미삭제 api 연결

--- src/apis/client.jsx
+++ src/apis/client.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api/spaceon", // 항상 Edge Function 경유
+  baseURL: "https://janghong.asia/", // 항상 Edge Function 경유
   headers: {
     "Content-Type": "application/json",
   },
