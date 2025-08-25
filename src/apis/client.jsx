b2d38d44c9ea377a62ab565b0272210a771cf334
Gyuho-Han <0203ho@naver.com>
2025-08-25 16:36:15 +0900

    fix: api 주소 / 제거

--- src/apis/client.jsx
+++ src/apis/client.jsx
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "https://janghong.asia/", // 항상 Edge Function 경유
+  baseURL: "https://janghong.asia", // 항상 Edge Function 경유
   headers: {
     "Content-Type": "application/json",
   },
